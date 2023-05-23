66b4146c85ae26ce452888262123f696148eaad6
Luis Alejandro <alfaroalejandro597@gmail.com>
2023-05-23 17:27:12 -0600

    Primer commit

---
+++ src/main/java/com/tienda_IIQK/tienda_IIQK/TiendaIiqkApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_IIQK.tienda_IIQK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiqkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiqkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda_IIQK/tienda_IIQK/TiendaIiqkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_IIQK.tienda_IIQK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIiqkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
